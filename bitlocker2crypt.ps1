f78f47f3648d1c4a86588d75d9c718ff006f6bad
nieivan <nie_ivan@hotmail.com>
2019-09-17 14:44:46 +0800

    Escrow all fixed drives recovery keys to Crypt-Server

--- bitlocker2crypt.ps1
+++ bitlocker2crypt.ps1
@@ -1,11 +1,13 @@
 <#	
 	.NOTES
 	===========================================================================
-	 Created by:   	Johnny Ramos
+   Created by:   	Johnny Ramos
+   Revised by:    Ivan Nie
 	 Filename:     	bitlocker2crypt.ps1
 	===========================================================================
 	.DESCRIPTION
-		Method to escrow a Windows Bitlocker key to Crypt-Server.
+    Method to escrow a Windows Bitlocker key to Crypt-Server.
+    Revision: Escrow all fixed drives recovery keys to Crypt-Server
 #>
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
@@ -17,13 +19,12 @@ $username = (& "$PSScriptRoot\Get-LoggedOnUser.ps1" -ComputerName $env:COMPUTERN
 
 $macname = $env:COMPUTERNAME
 
+#Obtain the list of fixed drive letters
 $FixedDrives = Get-WmiObject win32_diskdrive | Where-Object{$_.mediatype -eq "Fixed hard disk media"} | ForEach-Object{Get-WmiObject -Query "ASSOCIATORS OF {Win32_DiskDrive.DeviceID=`"$($_.DeviceID.replace('\','\\'))`"} WHERE AssocClass = Win32_DiskDriveToDiskPartition"} |  ForEach-Object{Get-WmiObject -Query "ASSOCIATORS OF {Win32_DiskPartition.DeviceID=`"$($_.DeviceID)`"} WHERE AssocClass = Win32_LogicalDiskToPartition"} | ForEach-Object{$_.deviceid}
-
 
 $bitLocker = Get-WmiObject `
                -Namespace "Root\cimv2\Security\MicrosoftVolumeEncryption" `
                -Class "Win32_EncryptableVolume" `
-               #-Filter "DriveLetter = '$env:SystemDrive'"
 
 $protector_id = $bitLocker.GetKeyProtectors("0").volumekeyprotectorID
 
@@ -32,7 +33,7 @@ Foreach ($FixedDrive in $FixedDrives){
   Foreach ($item in $protector_id) {
   $data = manage-bde -protectors -get $FixedDrive -id $item
   $key = ($data | Select-String -Pattern 'Password:' -Context 0,1 | ForEach-Object {$_.Context.PostContext})
-  
+
   if ($null -eq $key) {
     continue
   } else {
@@ -49,5 +50,4 @@ Foreach ($FixedDrive in $FixedDrives){
       }
     }
   }
-
 }
